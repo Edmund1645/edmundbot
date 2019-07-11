9ce183d816f4fdd43cbd04d2f8ba16b2e225d713
Edmund <edmund.timfon@gmail.com>
2019-07-11 01:14:39 +0100

    spin up jokes

--- modules/getJokes.js
+++ modules/getJokes.js
@@ -1,13 +1,30 @@
 const fetch = require('node-fetch');
-module.exports = {
-  categories: ['Programming', 'Miscellaneous'],
-  getCategory() {
-    const index = Math.round(Math.random() * (this.categories.length - 1));
-    return this.categories[index];
-  },
-  async getJoke() {
-    const url = `https://sv443.net/jokeapi/category/${this.getCategory()}?blacklistFlags=nsfw,religious,political`;
-    const data = await fetch(url)
-    return data;
-  }
-};
+const Twitter = require('./twitter');
+const categories = ['Programming', 'Miscellaneous'];
+function getCategory() {
+  const index = Math.round(Math.random() * (categories.length - 1));
+  return categories[index];
+}
+
+const url = `https://sv443.net/jokeapi/category/${getCategory()}?blacklistFlags=nsfw,religious,political`;
+const data = fetch(url);
+console.log(url);
+data
+  .catch(err => console.log(err))
+  .then(res => res.json())
+  .then(joke => {
+    const category =
+      joke.category == 'Programming'
+        ? '#programing #programmingJokes #techhumor'
+        : '#jokes #funny';
+    if ((joke.type = 'single')) {
+      Twitter.tweet(`${joke.joke} 
+      
+      ${category}`);
+    } else {
+      Twitter.tweet(`${joke.setup}
+      
+      ${joke.delivery}
+      ${category}`);
+    }
+  });
